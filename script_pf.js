85ab7eb6f8286397f4e66e42c8479668b2c09c98
novvmk <67250036+novvmk@users.noreply.github.com>
2024-09-06 02:22:24 +0100

    Add files via upload

---
+++ script_pf.js
@@ -0,0 +1,29 @@
+function checkPassword() {
+    var passwordInput = document.getElementById('passwordInput');
+    var password = passwordInput.value;
+    var correctPassword = 'CHOCOLATE';
+    var errorMessage = document.getElementById('errorMessage');
+
+    if (password === correctPassword) {
+        document.getElementById('passwordContainer').style.display = 'none';
+        document.getElementById('pfContainer').style.display = 'flex';
+    } else {
+        errorMessage.textContent = 'Incorrect password. Please try again.';
+        passwordInput.value = '';
+    }
+}
+
+document.getElementById('submitPassword').addEventListener('click', checkPassword);
+
+document.getElementById('passwordInput').addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        checkPassword();
+    }
+});
+
+document.getElementById('protectedVideo').addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
+
+
+
